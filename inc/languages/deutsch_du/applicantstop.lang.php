62e00fd308c8e5a0d272291fb12bc4fc8c3a93a9
saen91 <69750696+saen91@users.noreply.github.com>
2023-01-09 22:25:30 +0100

    Update applicantstop.lang.php

--- inc/languages/deutsch_du/applicantstop.lang.php
+++ inc/languages/deutsch_du/applicantstop.lang.php
@@ -1,11 +1,15 @@
 <?php
-//Allgemeines
-$l['applicantstop_name'] = "Aufnahmestops"; 
-$l['applicantstop_desc'] = "Willkommen im {$mybb->settings[\'bbname\']}. Hier findest du eine Übersicht über unsere Aufnahmestops.<br>Solltest du Fragen haben, kannst du dich gerne an uns wenden."
 
+//ALLGEMEINES
+$l['applicantstop_name'] = "Aufnahmestop"; 
+$l['applicantstop_welcome'] = "Hier findest du eine Übersicht über unsere Aufnahmestops.<br>Solltest du Fragen haben, kannst du dich gerne an uns wenden."; 
 
+$l['applicantstop_noend'] = "kein Enddatum bekannt"; 
+$l['applicantstop_active'] = "noch offen"; 
 
-
-
-
+$l['applicantstop_titel_solved'] = "Aufgehobene Stops"; //Titel für die aufgehobenen Stops im TPL applicantstop_solved
+$l['applicantstop_titel_active'] = "Aktuelle Stops";//Titel für die aktiven Stops im TPL applicantstop_active
+$l['applicantstop_beginn'] = "Beginn"; //Titel für Tabelle
+$l['applicantstop_art'] = "Art des Stops"; //Titel für Tabelle
+$l['applicantstop_ende'] = "Ende"; //Titel für Tabelle
 ?>
