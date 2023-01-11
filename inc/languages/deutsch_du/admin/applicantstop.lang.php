54d0db1e0eb35242a73707f1a1a07714738d9f52
saen91 <69750696+saen91@users.noreply.github.com>
2023-01-11 15:08:15 +0100

    Update applicantstop.lang.php

--- inc/languages/deutsch_du/admin/applicantstop.lang.php
+++ inc/languages/deutsch_du/admin/applicantstop.lang.php
@@ -1,23 +1,23 @@
 <?php
 //Beschreibung im ACP vom Plugin
-$l['applicantstop_name'] = "Aufnahmestop"; 
-$l['applicantstop_desc_acp'] = "Aufnahmestops über das ACP hinzufügen und im Forum ausgeben."; 
+$l['applicantstop_name'] = "Aufnahmestopp"; 
+$l['applicantstop_desc_acp'] = "Aufnahmestopps über das ACP hinzufügen und im Forum ausgeben."; 
 
 //Aufnahmestops hinzufügen, editieren & löschen
-$l['applicantstop_manage'] = "Aufnahmestop verwalten"; 
-$l['applicantstop_overview'] = "Aufnahmestop Übersicht"; 
-$l['applicantstop_add_entry'] = "Aufnahmestop hinzufügen";
-$l['applicantstop_add_entry_desc'] = "Neuen Aufnahmestop hinzufügen.";
-$l['applicantstop_add_entry_solved'] = "Ein neuer Stop wurde hinzugefügt.";
-$l['applicantstop_edit_entry'] = "Aufnahmestop bearbeiten";
+$l['applicantstop_manage'] = "Aufnahmestopp verwalten"; 
+$l['applicantstop_overview'] = "Aufnahmestopp Übersicht"; 
+$l['applicantstop_add_entry'] = "Aufnahmestopp hinzufügen";
+$l['applicantstop_add_entry_desc'] = "Neuen Aufnahmestopp hinzufügen.";
+$l['applicantstop_add_entry_solved'] = "Ein neuer Stopp wurde hinzugefügt.";
+$l['applicantstop_edit_entry'] = "Aufnahmestopp bearbeiten";
 $l['applicantstop_edit_entry_desc'] = "Hier bearbeitest du einen Aufnahmestop.";
-$l['applicantstop_edit_entry_solved'] = "Der Stop wurde bearbeitet.";
-$l['applicantstop_delete_entry_solved'] = "Der Stop wurde gelöscht.";
-$l['applicantstop_delete_entry_question'] = "Möchtest du den Aufnahmestop wirklich löschen?";
+$l['applicantstop_edit_entry_solved'] = "Der Stopp wurde bearbeitet.";
+$l['applicantstop_delete_entry_solved'] = "Der Stopp wurde gelöscht.";
+$l['applicantstop_delete_entry_question'] = "Möchtest du den Aufnahmestopp wirklich löschen?";
 
 //Allgemeines
 $l['applicantstop_overview_entries'] ="Übersicht"; 
-$l['applicantstop_overview_entries_desc'] = "Eine Übersicht aller eingetragener Aufnahmestops.";
+$l['applicantstop_overview_entries_desc'] = "Eine Übersicht aller eingetragener Aufnahmestopps.";
 $l['applicantstop_options'] = "Optionen";
 $l['applicantstop_edit'] = "Editieren";
 $l['applicantstop_delete'] = "Löschen";
@@ -25,24 +25,24 @@ $l['applicantstop_add'] = "Hinzufügen";
 $l['applicantstop_send'] = "Absenden";
 
 //Überschriften Overview Seite
-$l['applicantstop_overview_titel_titel'] = "Aufnahmestops";
+$l['applicantstop_overview_titel_titel'] = "Aufnahmestopps";
 $l['applicantstop_overview_titel_name'] = "Bezeichnung";
 $l['applicantstop_overview_titel_desc'] = "Beschreibung";
-$l['applicantstop_overview_titel_start'] = "Start des Stops";
-$l['applicantstop_overview_titel_end'] = "Ende des Stops";
+$l['applicantstop_overview_titel_start'] = "Start des Stopps";
+$l['applicantstop_overview_titel_end'] = "Ende des Stopps";
 $l['applicantstop_overview_titel_activesolved'] = "Aktiv/Beendet";
 
 //Formular
 $l['applicantstop_form_titel'] = "Titel";
-$l['applicantstop_form_titel_desc'] = "Wie heißt der Aufnahmestop?";
+$l['applicantstop_form_titel_desc'] = "Wie heißt der Aufnahmestopp?";
 $l['applicantstop_form_desc'] = "Beschreibung";
-$l['applicantstop_form_desc_desc'] = "Beschreibe hier den Aufnahmestop.";
+$l['applicantstop_form_desc_desc'] = "Beschreibe hier den Aufnahmestopp.";
 $l['applicantstop_form_start'] = "Startdatum";
 $l['applicantstop_form_start_desc'] = "Ab wann ist der Stop gültig?";
 $l['applicantstop_form_end'] = "Enddatum";
 $l['applicantstop_form_end_desc'] = "Trage hier das Enddatum ein. Ist es noch aktiv, dann musst du nichts eintragen.";
-$l['applicantstop_form_radio'] = "Handelt es sich um einen aktuellen Stop?";
-$l['applicantstop_form_radio_desc'] = "Wähle Nein, wenn dieser Aufnahmestop beendet ist. Bitte trage ein Enddatum ein, oder Lösche diesen Stop. ";
+$l['applicantstop_form_radio'] = "Handelt es sich um einen aktuellen Stopp?";
+$l['applicantstop_form_radio_desc'] = "Wähle Nein, wenn dieser Aufnahmestopp beendet ist. Bitte trage ein Enddatum ein, oder Lösche diesen Stopp. ";
 
 //errors
 $l['applicantstop_error_titel'] = "Kein Titel angelegt!";
