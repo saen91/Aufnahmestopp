241476f9adcfac60243f55ddc619656e7684862b
saen91 <69750696+saen91@users.noreply.github.com>
2023-01-11 15:09:12 +0100

    Update applicantstop.lang.php

--- inc/languages/deutsch_sie/applicantstop.lang.php
+++ inc/languages/deutsch_sie/applicantstop.lang.php
@@ -1,18 +1,18 @@
 <?php
 
 //ALLGEMEINES
-$l['applicantstop_name'] = "Aufnahmestop"; 
-$l['applicantstop_welcome'] = "Hier findest du eine Übersicht über unsere Aufnahmestops.<br>Solltest du Fragen haben, kannst du dich gerne an uns wenden."; 
+$l['applicantstop_name'] = "Aufnahmestopp"; 
+$l['applicantstop_welcome'] = "Hier findest du eine Übersicht über unsere Aufnahmestopps.<br>Solltest du Fragen haben, kannst du dich gerne an uns wenden."; 
 
 $l['applicantstop_noend'] = "kein Enddatum bekannt"; 
 $l['applicantstop_active'] = "noch offen"; 
 
-$l['applicantstop_titel_solved'] = "Aufgehobene Stops"; //Titel für die aufgehobenen Stops im TPL applicantstop_solved
-$l['applicantstop_titel_active'] = "Aktuelle Stops";//Titel für die aktiven Stops im TPL applicantstop_active
+$l['applicantstop_titel_solved'] = "Aufgehobene Stopps"; //Titel für die aufgehobenen Stops im TPL applicantstop_solved
+$l['applicantstop_titel_active'] = "Aktuelle Stopps";//Titel für die aktiven Stops im TPL applicantstop_active
 $l['applicantstop_beginn'] = "Beginn"; //Titel für Tabelle
-$l['applicantstop_art'] = "Art des Stops"; //Titel für Tabelle
+$l['applicantstop_art'] = "Art des Stopps"; //Titel für Tabelle
 $l['applicantstop_ende'] = "Ende"; //Titel für Tabelle
 
-$l['applicantstop_alert'] = "Es gibt neue <a href=\"applicantstop.php\">Aufnahmestops</a>"; //Text im Balken
+$l['applicantstop_alert'] = "Es gibt neue <a href=\"applicantstop.php\">Aufnahmestopps</a>"; //Text im Balken
 $l['applicantstop_alert_reading'] = "Als gelesen markieren"; //Text im Balken
 ?>
